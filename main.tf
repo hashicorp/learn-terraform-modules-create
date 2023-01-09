a1e29c431ddba10ff3123468af5d4bd95764aa71
Brian McClain <brianmmcclain@gmail.com>
2023-01-09 14:38:04 -0500

    Update to latest version of provider and modules

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.0.0"
+      version = "4.49.0"
     }
   }
 }
@@ -15,7 +15,7 @@ provider "aws" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "2.21.0"
+  version = "3.18.1"
 
   name = var.vpc_name
   cidr = var.vpc_cidr
@@ -31,10 +31,11 @@ module "vpc" {
 
 module "ec2_instances" {
   source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "2.12.0"
+  version = "4.3.0"
 
-  name           = "my-ec2-cluster"
-  instance_count = 2
+  count = 2
+
+  name           = "my-ec2-cluster-${count.index}"
 
   ami                    = "ami-0c5204531f799e0c6"
   instance_type          = "t2.micro"
