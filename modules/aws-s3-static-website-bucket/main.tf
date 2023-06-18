ae0b04690d04127427c30dfe52432b33316e00c3
kakakakakku <y.yoshida22@gmail.com>
2023-06-19 07:10:15 +0900

    fix: Update ACL to fix bucket creation issue

--- modules/aws-s3-static-website-bucket/main.tf
+++ modules/aws-s3-static-website-bucket/main.tf
@@ -21,13 +21,39 @@ resource "aws_s3_bucket_website_configuration" "s3_bucket" {
   }
 }
 
+resource "aws_s3_bucket_ownership_controls" "s3_bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_acl" "s3_bucket" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.s3_bucket,
+    aws_s3_bucket_public_access_block.s3_bucket,
+  ]
+
   bucket = aws_s3_bucket.s3_bucket.id
 
   acl = "public-read"
 }
 
 resource "aws_s3_bucket_policy" "s3_bucket" {
+  depends_on = [
+    aws_s3_bucket_acl.s3_bucket
+  ]
+
   bucket = aws_s3_bucket.s3_bucket.id
 
   policy = jsonencode({
